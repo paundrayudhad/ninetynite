fa4ae48016e39b30b0d47d06a8ce1f9ac8aa5cd4
Paundra Yudha Dharmawan <paundra461@gmail.com>
2024-09-04 15:42:03 +0700

    edit meta

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,17 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="description" content="Deskripsi Halaman" />
-        <meta name="keywords" content="kata kunci 1, kata kunci 2, kata kunci 3" />
-        <meta name="author" content="Nama Penulis" />
-        <meta property="og:title" content="Judul untuk Open Graph" />
-        <meta property="og:description" content="Deskripsi untuk Open Graph" />
-        <meta property="og:image" content="url-gambar.jpg" />
-        <meta property="og:url" content="https://contoh.com/halaman" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Judul untuk Twitter" />
-        <meta name="twitter:description" content="Deskripsi untuk Twitter" />
-        <meta name="twitter:image" content="url-gambar-twitter.jpg" />
+        <meta name="description" content="Temukan koleksi baju eksklusif dari brand Ninetynite+ di Surakarta. Fashion terbaru dan tren terkini, hanya untuk Anda. Belanja sekarang dan tampil stylish dengan Ninetynite+." />
+        <meta name="keywords" content="brand baju Ninetynite+ Surakarta, baju Ninetynite+ Surakarta, fashion Ninetynite+ Surakarta, koleksi baju Ninetynite+ Surakarta, Ninetynite+ Surakarta, pakaian Ninetynite+ Surakarta, Ninetynite+ fashion Surakarta, jual baju Ninetynite+ Surakarta" />
+        <meta name="author" content="Paundra Yudha Dharmawan, Marshal Pasha Kurniawan" />
+        <meta property="og:title" content="Ninetynite+" />
+        <meta property="og:description" content="Temukan koleksi baju eksklusif dari brand Ninetynite+ di Surakarta. Fashion terbaru dan tren terkini, hanya untuk Anda. Belanja sekarang dan tampil stylish dengan Ninetynite+." />
+        <meta property="og:url" content="https://ninetynite.vercel.app/" />
       </Head>
       <body>
         <Main />
