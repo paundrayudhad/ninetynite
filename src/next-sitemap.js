d8c7dbdea6e98310a9259f98ec6cc3d28d534075
Paundra Yudha Dharmawan <paundra461@gmail.com>
2024-09-05 13:52:31 +0700

    .

---
+++ src/next-sitemap.js
@@ -0,0 +1,7 @@
+// next-sitemap.js
+module.exports = {
+    siteUrl: 'https://ninetynite.vercel.app/', // Ganti dengan URL situs Anda
+    generateRobotsTxt: true, // (Opsional) Jika Anda juga ingin menghasilkan robots.txt
+    // Anda bisa menambahkan pengaturan lain sesuai kebutuhan
+  };
+  
