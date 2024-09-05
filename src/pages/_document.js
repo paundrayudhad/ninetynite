4b4ab01471033c33e15e55254fc41f984eef94ab
Paundra Yudha Dharmawan <paundra461@gmail.com>
2024-09-05 13:52:19 +0700

    .

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="description" content="Temukan koleksi baju eksklusif dari brand Ninetynite+ di Surakarta. Fashion terbaru dan tren terkini, hanya untuk Anda. Belanja sekarang dan tampil stylish dengan Ninetynite+." />
-        <meta name="keywords" content="brand baju Ninetynite+ Surakarta, baju Ninetynite+ Surakarta, fashion Ninetynite+ Surakarta, koleksi baju Ninetynite+ Surakarta, Ninetynite+ Surakarta, pakaian Ninetynite+ Surakarta, Ninetynite+ fashion Surakarta, jual baju Ninetynite+ Surakarta" />
+        <meta name="keywords" content="brand baju Ninetynite+ Surakarta, baju Ninetynite+ Surakarta, fashion Ninetynite+ Surakarta, koleksi baju Ninetynite+ Surakarta, Ninetynite+ Surakarta, pakaian Ninetynite+ Surakarta, Ninetynite+, jual baju Ninetynite+ Surakarta" />
         <meta name="author" content="Paundra Yudha Dharmawan, Marshal Pasha Kurniawan" />
         <meta property="og:title" content="Ninetynite+" />
         <meta property="og:description" content="Temukan koleksi baju eksklusif dari brand Ninetynite+ di Surakarta. Fashion terbaru dan tren terkini, hanya untuk Anda. Belanja sekarang dan tampil stylish dengan Ninetynite+." />
