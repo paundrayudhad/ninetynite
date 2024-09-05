62e4ba0f7bf324e56cc0ff4a6fb20be2775e5fe3
Paundra Yudha Dharmawan <paundra461@gmail.com>
2024-09-05 11:08:33 +0700

    .

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,38 +5,37 @@ import GradualSpacing from "./magicui/gradual-spacing";
 import BlurIn from "./magicui/blur-in";
 
 
-export default function hero() {
+export default function Hero() {
   return (
     <>
-    <section className="bg-neutral-950 min-h-screen flex items-center justify-center">
-      <div className="max-w-screen-lg flex flex-col items-center text-center -ml-10">
-        <BlurIn 
-          className="text-white bg-clip-text lg:text-[14rem] text-8xl md:text-[120px] font-embassy z-10"
-          word="Ninetynite"
-        >
-      </BlurIn>
-      <GridPattern
-        squares={[
-          [4, 4],
-          [5, 1],
-          [8, 2],
-          [5, 3],
-          [5, 5],
-          [10, 10],
-          [12, 15],
-          [15, 10],
-          [10, 15],
-          [15, 10],
-          [10, 15],
-          [15, 10],
-        ]}
-        className={cn(
-          "[mask-image:radial-gradient(400px_circle_at_center,white,transparent)]",
-          "inset-x-0 inset-y-[-30%] h-[200%] skew-y-12",
-        )}
-      />
-  </div>
-</section>
-</>
-  )
+      <section className="bg-neutral-950 min-h-screen flex items-center justify-center">
+        <div className="max-w-screen-lg w-full flex flex-col items-center text-center pr-7">
+          <BlurIn 
+            className="text-white bg-clip-text lg:text-[14rem] text-6xl md:text-[8rem] font-embassy z-10"
+            word="Ninetynite"
+          />
+          <GridPattern
+            squares={[
+              [4, 4],
+              [5, 1],
+              [8, 2],
+              [5, 3],
+              [5, 5],
+              [10, 10],
+              [12, 15],
+              [15, 10],
+              [10, 15],
+              [15, 10],
+              [10, 15],
+              [15, 10],
+            ]}
+            className={cn(
+              "[mask-image:radial-gradient(400px_circle_at_center,white,transparent)]",
+              "inset-x-0 inset-y-[-30%] h-[200%] skew-y-12",
+            )}
+          />
+        </div>
+      </section>
+    </>
+  );
 }
